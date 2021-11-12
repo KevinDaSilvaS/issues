8bfbc8458781c41d536a391124a1953263f22084
Your Name <kevin.kds80@gmail.com>
2021-11-12 23:27:25 +0000

    refactored parse_args function and add process

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,17 +6,28 @@ defmodule Issues.CLI do
   end
 
   def parse_args(argv) do
-    parse = OptionParser.parse argv, switches: [help: :boolean],
-                                     aliases: [h: :help]
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {
-          user,
-          project,
-          String.to_integer(count)
-        }
-      {_, [user, project], _} -> {user, project, @default_count}
-      _ -> :help
-    end
+    OptionParser.parse(argv, switches: [help: :boolean],
+                                     aliases: [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation()
+    |> process()
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+  def args_to_internal_representation(_), do: :help
+
+  def process(:help) do
+    IO.puts """
+      usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt
+  end
+  def process({user, project, count}) do
+    IO.puts "U -> #{user}, P -> #{project}, C -> #{count}"
   end
 end
